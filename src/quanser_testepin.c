785383258f806f9b9bf32788fab220da7f7fe1ad
Francisco Knebel <franciscopaivaknebel@gmail.com>
2019-07-05 19:38:18 -0300

    testepin string.

--- src/quanser_testepin.c
+++ src/quanser_testepin.c
@@ -1,16 +1,37 @@
+// Quanser Shield v.2
+// Copyright (C) 2019 Francisco Knebel, Luciano Zancan, Rodrigo Dal Ri
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * @file quanser_testepin.c
+ * @author Francisco Knebel, Luciano Zancan, Rodrigo Dal Ri
+ * @date 26 Jun 2019
+ * @brief Receive a gpio pin and tests it.
+ */
+
+#include <quanser_pwm.h>
 
 int main(int argc, char const *argv[]){
-
-  gpio = argv[1];
+  int gpio = strtol(argv[1], NULL, 10);
   char buffer[1];
 
   while(1){
+    char str[50];
+    sprintf(str, "/sys/class/gpio/gpio%d/value", gpio);
+    pgets(buffer, 1, str);
 
-  pgets(buffer, 1, "/sys/class/gpio/gpio/value");
-
-
-
+    return 0;
   }
-
-
 }
